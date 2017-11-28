ec9ce612cda10db635781f9d567570691506149c
embs <embs@cin.ufpe.br>
2017-11-28 17:32:26 -0300

    Set Content-Length header in responses

---
+++ src/main/java/works/weave/socks/shipping/configuration/FilterConfig.java
@@ -0,0 +1,20 @@
+package works.weave.socks.shipping.configuration;
+
+import java.util.Arrays;
+
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(new ShallowEtagHeaderFilter());
+        filterBean.setUrlPatterns(Arrays.asList("*"));
+        return filterBean;
+    }
+}
